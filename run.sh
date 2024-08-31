9827dc3f892e656959aeeee2902f27204ffd79a1
Ibrahim Ercan <ibrahim.metu@gmail.com>
2024-08-31 14:26:31 +0000

    set remote access via command line option

---
+++ run.sh
@@ -0,0 +1,8 @@
+EXTRA_FLAGS=''
+if [[ $ALLOW_REMOTE_ACCESS == "yes" ]];then
+    EXTRA_FLAGS="$EXTRA_FLAGS --bind-all"
+fi
+
+/opt/acestream/start-engine --client-console --http-port $HTTP_PORT $EXTRA_FLAGS
+
+
